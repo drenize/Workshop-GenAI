6a6860873c232e56ce46d188696c05c68d79ba58
Fabian Kaiser <fabian.kaiser@analytical-software.de>
2024-09-12 18:14:24 +0200

    initial

---
+++ README.md
@@ -0,0 +1,28 @@
+# Workshop GenAI
+
+In this repository you can find starter code and data for the Workshop GenAI!
+
+Please create a virtual environment and install the requirements.txt:
+
+```
+python -m venv venv
+venv/Scripts/activate
+pip install -r requirements.txt
+```
+
+Make sure to create a .env file with 
+
+```
+AZURE_OPENAI_KEY=
+AZURE_OPENAI_ENDPOINT=
+AZURE_OPENAI_DEPLOYMENT=
+AZURE_OPENAI_EMBEDDINGS=
+AZURE_OPENAI_WHISPER=
+
+```
+
+which will be provided for you.
+
+# Data
+
+Audio, Data and Images are respective folders for the audio, text and image data for the workshop.
---
+++ chat.py
@@ -0,0 +1,63 @@
+import os
+import streamlit as st
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.prompts import ChatPromptTemplate
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain_core.messages.ai import AIMessage
+from langchain_openai.chat_models import AzureChatOpenAI
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+st.title("Chatbot")
+
+azure_version = "2024-06-01"
+azure_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+azure_key = os.getenv("AZURE_OPENAI_KEY")
+
+llm = AzureChatOpenAI(
+    temperature=0.1,
+    top_p=1.0,
+    azure_deployment=azure_deployment,
+    api_key=azure_key,
+    azure_endpoint=azure_endpoint,
+    api_version="2024-06-01",
+)
+
+# Set up memory
+if 'memory' not in st.session_state:
+    message = AIMessage("What can I help you with today?")
+    st.session_state.memory = ConversationBufferWindowMemory(
+        chat_memory=ChatMessageHistory(messages=[message]),
+        memory_key='chat_history',
+        k=20,
+        return_messages=True,
+        human_prefix="Human",
+        ai_prefix="AI",
+    )
+
+chat_prompt = ChatPromptTemplate.from_template(
+    """
+Answer the question.
+{input}
+with the history
+{chat_history}
+""")
+
+chain = chat_prompt | llm
+
+# Render current messages from StreamlitChatMessageHistory
+for msg in st.session_state.memory.buffer_as_messages:
+    st.chat_message(msg.type).write(msg.content)
+
+if prompt := st.chat_input():
+    st.chat_message("human").write(prompt)
+    # Note: new messages are saved to history automatically by Langchain during run
+    response = chain.invoke({
+        "input": prompt,
+        "chat_history": st.session_state.memory.buffer_as_messages,
+    })
+    st.chat_message("ai").write(response.content)
